cd0c6f9cf3cebd59079007aeb8cab2ab2ae7ee54
Dokyun Lim <im.koremp@gmail.com>
2022-12-07 12:36:09 +0900

    chore: add ssr to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import ssr from 'vite-plugin-ssr/plugin'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ssr()],
   server: {
     port: 3000,
     host: '127.0.0.1',
