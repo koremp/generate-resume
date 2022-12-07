4827082ae3e194ae3dab80b9227e5a74e8e6e95f
Dokyun Lim <im.koremp@gmail.com>
2022-12-07 15:19:31 +0900

    refact: fix index page to basic

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,19 +1,7 @@
-import React from 'react'
-import { Counter } from './Counter'
-
-export { Page }
-
-function Page() {
+export default function Page() {
   return (
-    <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
+    <div>
+      <h1>Index Page</h1>
+    </div>
   )
 }
